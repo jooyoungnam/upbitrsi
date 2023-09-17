42b5a44d337286b212826833ee827b5936abc011
whitelang <143645324+jooyoungnam@users.noreply.github.com>
2023-09-17 21:00:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,65 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const path = require('path');
-
 const app = express();
 const port = 3000;
 
+// 정적 파일을 제공할 디렉토리 설정
+app.use(express.static('public'));
+
+// 캐싱된 RSI와 마지막 업데이트 시간을 저장할 변수
 let cachedRSI = {};
-let marketIndex = 0;
 let krwMarkets = [];
+let lastUpdated = null;
 
+// CORS 미들웨어 사용 설정
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/rsi', (req, res) => {
-  res.json({ rsiData: cachedRSI });
+// 기본 라우트 설정
+app.get('/', (req, res) => {
+  res.send('Upbit RSI API server.');
 });
 
+// 업비트에서 모든 시장 데이터를 가져와 KRW로 시작하는 시장만 필터링
 async function loadMarkets() {
-  try {
-    const marketResponse = await axios.get('https://api.upbit.com/v1/market/all');
-    krwMarkets = marketResponse.data
-      .filter(m => m.market.startsWith('KRW'))
-      .map(m => ({ market: m.market, korean_name: m.korean_name }));
-  } catch (error) {
-    console.error("마켓 정보 로딩 중 에러:", error);
-  }
+  const response = await axios.get('https://api.upbit.com/v1/market/all');
+  krwMarkets = response.data
+    .filter(market => market.market.startsWith('KRW'))
+    .map(market => ({ market: market.market, korean_name: market.korean_name }));
 }
 
-async function updateOneMarketRSI() {
-  if (krwMarkets.length === 0) {
-    console.error("마켓 정보가 비어 있습니다.");
-    return;
-  }
-
-  try {
-    const market = krwMarkets[marketIndex];
-    const rsi = await getRSI(market.market);
-    cachedRSI[market.market] = { rsi, korean_name: market.korean_name };
-
-    marketIndex = (marketIndex + 1) % krwMarkets.length;
-  } catch (error) {
-    console.error("RSI 업데이트 중 에러:", error);
-  }
-}
-
+// RSI 계산 함수
 async function getRSI(market, interval = 240) {
+  // 200개의 캔들 데이터를 가져옴
   const count = 200;
   const url = `https://api.upbit.com/v1/candles/minutes/${interval}?market=${market}&count=${count}`;
   const response = await axios.get(url);
   const data = response.data.reverse();
- 
-  let upList = [];
-  let downList = [];
+  
+  const upList = [];
+  const downList = [];
+  const zero = 0;
 
-  const n = 14;
-  const a = 1 / (1 + (n - 1));
-
+  // 캔들간 가격 차이 계산
   for (let i = 0; i < data.length - 1; i++) {
-    const gap = data[i + 1].trade_price - data[i].trade_price;
-
-    if (gap > 0) {
-      upList.push(gap);
-      downList.push(0);
-    } else if (gap < 0) {
-      upList.push(0);
-      downList.push(-gap);
+    const gapByTradePrice = data[i + 1].trade_price - data[i].trade_price;
+    
+    if (gapByTradePrice > 0) {
+      upList.push(gapByTradePrice);
+      downList.push(zero);
+    } else if (gapByTradePrice < 0) {
+      downList.push(-gapByTradePrice);
+      upList.push(zero);
     } else {
-      upList.push(0);
-      downList.push(0);
+      upList.push(zero);
+      downList.push(zero);
     }
   }
-
+  
+  const day = 14;
+  const a = 1 / (1 + (day - 1));
+  
+  // 지수 이동 평균 계산
   let upEma = upList[0];
   for (let i = 1; i < upList.length; i++) {
     upEma = (upList[i] * a) + (upEma * (1 - a));
@@ -82,33 +70,46 @@ async function getRSI(market, interval = 240) {
     downEma = (downList[i] * a) + (downEma * (1 - a));
   }
 
+  // RSI 계산
   const rs = upEma / downEma;
   const rsi = 100 - (100 / (1 + rs));
 
   return rsi;
 }
 
-setInterval(() => {
-  updateOneMarketRSI();
-}, 1 * 60 * 1000);
+// RSI 업데이트 함수
+async function updateRSI() {
+  for (const market of krwMarkets) {
+    try {
+      const rsi = await getRSI(market.market);
+      if (rsi !== null) {
+        cachedRSI[market.market] = { rsi, korean_name: market.korean_name };
+      }
+    } catch (error) {
+      console.error(`Failed to update RSI for ${market.market}`, error);
+    }
+    await new Promise(resolve => setTimeout(resolve, 200));
+  }
+  
+  // 마지막 업데이트 시간 저장
+  lastUpdated = new Date().toISOString();
+  
+  // RSI 값에 따라 내림차순 정렬
+  cachedRSI = Object.fromEntries(
+    Object.entries(cachedRSI).sort(([,a], [,b]) => b.rsi - a.rsi)
+  );
+}
 
-loadMarkets().then(() => {
-  updateOneMarketRSI();
+// RSI 데이터 반환 라우트
+app.get('/rsi', (req, res) => {
+  res.json({ rsiData: cachedRSI, lastUpdated });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
+// 서버 시작
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
-});
-
-setInterval(() => {
-  console.log("Resetting RSI data...");
-  cachedRSI = {};
-}, 1000 * 60 * 60 * 24 * 7);
-
-loadMarkets().then(() => {
-  updateOneMarketRSI();
+  loadMarkets().then(() => {
+    updateRSI();
+    setInterval(updateRSI, 60 * 1000); // 1분마다 RSI 업데이트
+  });
 });
