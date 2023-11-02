8d53210f6c671f12a7080bb511a730aa89746bc2
whitelang <143645324+jooyoungnam@users.noreply.github.com>
2023-11-02 16:38:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.static('public'));
 
